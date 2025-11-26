5575e95d0d1c54fdb997e962db8732193edf81f2
Franco García <francoagf13@mail.com>
2025-11-25 21:11:06 -0300

    Feat: Dog Jump, Removed Unnecesary Code
    -El perro ahora tiene un código que le permite saltar. Idealmente se puede automatizar.
    -Se removieron líneas de código innecesarias (Curación, valores de daño diferentes, limitador de HP máximo, etc...)
    -Se adaptó a la nueva visión del juego (Todo lo que te puede matar hace 1 de daño y te mata inmediatamente)

--- Assets/Scenes/Test/PlayerHP.cs
+++ Assets/Scenes/Test/PlayerHP.cs
@@ -3,54 +3,35 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
-public int CurrentHp = 5;
-public int MaxHp = 5;
+public int CurrentHp = 1;
     void Update() {
 
-      /*Llama a las funciones de recibir daño y curar
-       El valor cambia dependiendo de la fuente
-       Pare recibir daño el objeto necesita un tag
-       "Doggy" para el perro (instakill)
-       "Heart" para los corazones (+1hp)
-       "Obstacle" para los obstáculos (-1hp)*/
-      void onTriggerEnter2D(Collider other) {
-        if (other.CompareTag("Doggy")) {
-          TakeDamage(5);
-        }
-
-        if (other.CompareTag("Heart")) {
-          IsHealing(1);
-        }
-
-        if (other.CompareTag("Obstacle")) {
+      /*Detecta si colisiona con un objeto que haga daño
+       Si colisiona llama a la función de hacer daño
+       WIP*/
+      void onTriggerEnter2D(Collider other)
+      {
+        if (other.CompareTag("DamagingObject"))
+        {
           TakeDamage(1);
         }
 
-
       }
-
-      //Reduce HP si el jugador recibe daño
-      void TakeDamage(int amount) {
+      //Reduce la vida del jugador
+      void TakeDamage(int amount)
+      {
         CurrentHp -= amount;
       }
 
-      //Mata al jugador si su HP es 0
-        if (CurrentHp <= 0) {
+      //Manda a matar al jugador
+        if (CurrentHp <= 0)
+        {
           Die();
         }
-
-        //Evita que el jugador pase de 5HP
-        if (CurrentHp >= MaxHp) {
-          CurrentHp = MaxHp;
-        }
-
-        //Aumenta HP si el jugador se cura
-        void IsHealing(int amount) {
-          CurrentHp += amount;
-        }
     }
-
-    void Die() {
+      //Mata al jugador
+    void Die()
+    {
       Destroy(gameObject);
     }
 }
--- Assets/Scripts/DogMovement.cs
+++ Assets/Scripts/DogMovement.cs
@@ -1,10 +1,42 @@
+using System;
 using UnityEngine;
 
 public class DogMovement : MonoBehaviour {
   public float DogSpeed = 10f;
-//Wip, todavia no esta funcionando
-    void Update()
+  private Rigidbody2D rb;
+  public float JumpForce;
+
+
+  private void Start() {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+
+  void Update() {
+    //Movimiento horizontal constante
+    transform.position += new Vector3(DogSpeed, 0, 0) * Time.deltaTime;
+
+    //Esto es de prueba nada más, luego se puede borrar
+    //funciona mejor si gravity scale = 3 (Dentro de Rigidbody2D en Unity)
+    if (Input.GetKey(KeyCode.Space))
     {
-    transform.position += new Vector3(DogSpeed, 0, 0) *  Time.deltaTime;
+      rb.AddForce(Vector2.up * 100);
     }
+  /*Si detecta que está dentro de "AreaDeSalto" hace un salto
+   la idea es que "AreaDeSalto" sea un rango alrededor del obstáculo (sin colisiones)
+   Entonces al entrar en contacto ejecuta el salto automáticamente
+   */
+    void OnTriggerEnter2D(Collider2D other) {
+      if (other.CompareTag("AreaDeSalto")) {
+        Jump();
+      }
+    }
+
+    //Ejecuta el salto
+    void Jump()
+    {
+      rb.AddForce(Vector2.up * 100);
+    }
+
+  }
 }
