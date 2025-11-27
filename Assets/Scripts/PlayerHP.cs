776b68836a9e8a6811c14d5459ca9014b3bd3e93
Bruno Tejeria <brunotejeria@hotmail.com>
2025-11-27 09:14:24 -0300

    feat: Arreglar Muerte de jugador

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -1,37 +1,53 @@
-
+using System.Linq;
 using UnityEngine;
 
-public class PlayerHP : MonoBehaviour
-{
-public int CurrentHp = 1;
-    void Update() {
+// Segun chatGPT es buena practica, hay otras formas de declarar tipos pero es la unica que supe aplicar y que funcione
+public struct Damage {
+  public string Key { get; set; }
+  public float Amount { get; set; }
+}
 
-      /*Detecta si colisiona con un objeto que haga daño
-       Si colisiona llama a la función de hacer daño
-       WIP*/
-      void onTriggerEnter2D(Collider other)
-      {
-        if (other.CompareTag("DamagingObject"))
-        {
-          TakeDamage(1);
-        }
 
-      }
-      //Reduce la vida del jugador
-      void TakeDamage(int amount)
-      {
-        CurrentHp -= amount;
-      }
+public class PlayerHP : MonoBehaviour {
+  public float CurrentHp = 1;
 
-      //Manda a matar al jugador
-        if (CurrentHp <= 0)
-        {
-          Die();
-        }
+  private readonly Damage[] Damages = {
+    new() {
+      Key = "DamagingObject",
+      Amount = 1f
     }
-      //Mata al jugador
-    void Die()
-    {
-      Destroy(gameObject);
+    // Aquí van mas daños que se agreguen en un futuro
+  };
+
+
+  private void Update() {
+    //Manda a matar al jugador
+    if (CurrentHp <= 0) {
+      Die();
     }
+  }
+
+  /* Detecta si colisiona con un objeto que haga daño
+ Si colisiona llama a la función de hacer daño */
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    // Buscar si hay algun Damage con ese tag
+    var damage = Damages.FirstOrDefault(d => other.CompareTag(d.Key));
+    if (damage.Amount > 0f) {
+      TakeDamage(damage.Amount);
+    }
+  }
+
+  /*
+   * IMPORTANTE: Mantener estos métodos publicos.
+   */
+  //Reduce la vida del jugador
+  public void TakeDamage(float amount) {
+    CurrentHp -= amount;
+  }
+
+  //Mata al jugador
+  public void Die() {
+    Destroy(gameObject);
+  }
 }
